705223f7bdcc783ff75db314837740ae155feafd
Joaquin Salazar <joaquinsalazar@Joaquins-MBP.lan>
2023-08-08 23:54:05 -0400

    ejercicio de teoria

---
+++ Sem1/teo/ejercicio1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+//En esta oportunidad hablaremos de los pointers
+template<typename T>
+T chamoy(T a, T b){
+    return a+b;
+}
+
+int main(){
+    cout<<"mi chamoy"<<endl;
+    cout<<chamoy(int('A'),int('B'));
+    return 0;
+}
